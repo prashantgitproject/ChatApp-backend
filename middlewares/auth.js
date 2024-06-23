c4d56c84b354a314b3b23b352afc475997e112aa
Prashant SIngh <prashant.sb4754@gmail.com>
2024-06-23 12:22:25 +0530

    vercel update 1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
-import { adminSecretKey } from "../app.js";
+import { adminSecretKey } from "../index.js";
 import { ErrorHandler } from "../utils/utility.js";
 import jwt from 'jsonwebtoken';
 import { TryCatch } from "./error.js";
