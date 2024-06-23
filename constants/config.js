e67421830100ae2d8f149b914ea3cddba50eccc5
Prashant SIngh <prashant.sb4754@gmail.com>
2024-06-23 12:00:13 +0530

    added vercel.json

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,10 @@
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL,
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      process.env.CLIENT_URL,
+    ],
     credentials: true,
   }
 
